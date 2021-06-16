3c5bcdbb5a1e4f1b88e654628b374876052d17ba
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-06-17 01:51:48 +0530

    feat: delete a note

--- src/App.js
+++ src/App.js
@@ -32,9 +32,18 @@ const App = () => {
     setNotes(newNotes);
   };
 
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
     <div className='container'>
-      <NotesList notes={notes} handleAddNote={addNote} />
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
     </div>
   );
 };
