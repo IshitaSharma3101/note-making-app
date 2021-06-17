b22e533579eec6479fe2fbe85e92beed6f3e5e33
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-06-17 20:59:10 +0530

    feat: sort notes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
@@ -9,17 +9,27 @@ const App = () => {
     {
       id: nanoid(),
       text: "This is my first note",
-      date: "16/06/2021",
+      date: "11/06/2021",
     },
     {
       id: nanoid(),
       text: "This is my second note",
-      date: "16/06/2021",
+      date: "16/04/2021",
     },
     {
       id: nanoid(),
       text: "This is my third note",
-      date: "16/06/2021",
+      date: "19/06/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "01/06/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "11/12/2021",
     },
   ]);
 
@@ -42,14 +52,37 @@ const App = () => {
     setNotes(newNotes);
   };
 
+  const oldestFirst = () => {
+    notes.sort(
+      (a, b) =>
+        new Date(...a.date.split("/").reverse()) -
+        new Date(...b.date.split("/").reverse()),
+    );
+    setNotes([...notes]);
+    console.log(notes);
+  }
+
+  const newestFirst = () => {
+    notes.sort(
+      (a, b) =>
+        new Date(...b.date.split("/").reverse()) -
+        new Date(...a.date.split("/").reverse()),
+    );
+    setNotes([...notes]);
+  };
+
   return (
     <div className={`${darkMode && "dark-mode"}`}>
       <div className='container'>
-        <Header handleToggleDarkMode={setDarkMode} />
+        <Header
+          handleToggleDarkMode={setDarkMode}
+          handleNewestFirst={newestFirst}
+          handleOldestFirst={oldestFirst}
+        />
         <Search handleSearchNote={setSearchText} />
         <NotesList
           notes={notes.filter((note) =>
-            note.text.toLowerCase().includes(searchText),
+            note.text.toLowerCase().includes(searchText.toLowerCase()),
           )}
           handleAddNote={addNote}
           handleDeleteNote={deleteNote}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
-const Header = ({ handleToggleDarkMode }) => {
+const Header = ({
+  handleToggleDarkMode,
+  handleNewestFirst,
+  handleOldestFirst,
+}) => {
   return (
     <div className='header'>
       <h1>Note Making App</h1>
-      <button
-        className='save'
-        onClick={() =>
-          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
-        }>
-        Toggle Mode
-      </button>
+      <div className='nav'>
+        <button className='save' onClick={() => handleNewestFirst()}>
+          Newest First
+        </button>
+        <button className='save' onClick={() => handleOldestFirst()}>
+          Oldest First
+        </button>
+        <button
+          className='save'
+          onClick={() =>
+            handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+          }>
+          Toggle Mode
+        </button>
+      </div>
     </div>
   );
 };
